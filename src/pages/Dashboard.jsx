eaa44b551ed2c304994c9d4e1037e1244e1d62bd
Khushi SIngh <khushi.singh1661@gmail.com>
2026-01-28 21:28:14 +0530

    Fix import statement for NavBar component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import NavBar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 import NotesPanel from "../components/NotesPanel";
 import FlashcardBox from "../components/FlashcardBox";
 import PomodoroMini from "../components/PomodoroMini";
